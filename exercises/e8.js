c0a1030ae3fb5202c13c79e68baf29984ce676bb
keanneesteban <keanneesteban7@gmail.com>
2023-07-18 09:30:40 -0500

    completed exercise 8

--- exercises/e8.js
+++ exercises/e8.js
@@ -5,10 +5,45 @@
 // getClientWithGreatestBalance(bankAccounts) => [{ name: 'SomeName', balance: 32, ... }]
 
 export function getClientWithGreatestBalance(array) {
-  // Your code goes here...
+  var account = null;
+  var highestBalance = -Infinity;
 
+  for (var i = 0; i < array.length; i++) {
+    if (array[i].balance > 0 && array[i].balance >= highestBalance) {
+      highestBalance = array[i].balance;
+      account = array[i];
+    }
+  }
+
+  if (account === null) {
+    return [];
+  }
+
+  return [account];
 }
 
+
+const bankAccounts = [
+  {
+    id: 1,
+    name: "Susan",
+    balance: 100.32,
+    deposits: [150, 30, 221],
+    withdrawals: [110, 70.68, 120],
+  },
+  { id: 2, name: "Morgan", balance: 1100.0, deposits: [1100] },
+  {
+    id: 3,
+    name: "Joshua",
+    balance: 18456.57,
+    deposits: [4000, 5000, 6000, 9200, 256.57],
+    withdrawals: [1500, 1400, 1500, 1500],
+  },
+  { id: 4, name: "Candy", balance: 0.0 },
+  { id: 5, name: "Phil", balance: 18, deposits: [100, 18], withdrawals: [100] },
+];
+
+console.log(getClientWithGreatestBalance(bankAccounts));
 
 
 // === TEST YOURSELF ===
