f62933a7df4f811dc21d86521da61eb85251d718
keanneesteban <keanneesteban7@gmail.com>
2023-07-18 13:27:16 -0500

    completed exercise 11

--- exercises/e11.js
+++ exercises/e11.js
@@ -5,9 +5,43 @@
 // getAllWithdrawals(bankAccounts) => [3432, 0, 43242.34, 0, 23432]
 
 export function getAllWithdrawals(array) {
-  // Your code goes here...
+  var withdrawalSums = [];
 
+  for (let i = 0; i < array.length; i++) {
+    var withdrawals = array[i].withdrawals;
+
+    if (withdrawals && withdrawals.length > 0) {
+      var sum = withdrawals.reduce((total, withdrawal) => total + withdrawal, 0);
+      withdrawalSums.push(sum);
+    } else {
+      withdrawalSums.push(0);
+    }
+  }
+
+  return withdrawalSums;
 }
+
+const bankAccounts = [
+  {
+    id: 1,
+    name: "Susan",
+    balance: 100.32,
+    deposits: [150, 30, 221],
+    withdrawals: [110, 70.68, 120],
+  },
+  { id: 2, name: "Morgan", balance: 1100.0, deposits: [1100] },
+  {
+    id: 3,
+    name: "Joshua",
+    balance: 18456.57,
+    deposits: [4000, 5000, 6000, 9200, 256.57],
+    withdrawals: [1500, 1400, 1500, 1500],
+  },
+  { id: 4, name: "Candy", balance: 0.0 },
+  { id: 5, name: "Phil", balance: 18, deposits: [100, 18], withdrawals: [100] },
+];
+
+console.log(getAllWithdrawals(bankAccounts));
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-11"
