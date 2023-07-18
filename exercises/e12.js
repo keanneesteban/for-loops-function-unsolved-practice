07ce89b0bc1c5c988d3d894580231d8aed12277e
keanneesteban <keanneesteban7@gmail.com>
2023-07-18 15:46:51 -0500

    completed exercise 12

--- exercises/e12.js
+++ exercises/e12.js
@@ -4,10 +4,43 @@
 // Array example: bankAccounts in /data/data.js
 // getAllDepositsGreaterThanOneHundred(bankAccounts) => [3432, 43242.34, 23432]
 
-export function getAllDepositsGreaterThanOneHundred(array) {
-  // Your code goes here...
+ export function getAllDepositsGreaterThanOneHundred(array) {
+  var deposits = [];
 
+  for (var i = 0; i < array.length; i++) {
+    if (array[i].deposits) {
+      for (var j = 0; j < array[i].deposits.length; j++) {
+        if (array[i].deposits[j] > 100) {
+          deposits.push(array[i].deposits[j]);
+        }
+      }
+    }
+  }
+
+  return deposits;
 }
+
+const bankAccounts = [
+  {
+    id: 1,
+    name: "Susan",
+    balance: 100.32,
+    deposits: [150, 30, 221],
+    withdrawals: [110, 70.68, 120],
+  },
+  { id: 2, name: "Morgan", balance: 1100.0, deposits: [1100] },
+  {
+    id: 3,
+    name: "Joshua",
+    balance: 18456.57,
+    deposits: [4000, 5000, 6000, 9200, 256.57],
+    withdrawals: [1500, 1400, 1500, 1500],
+  },
+  { id: 4, name: "Candy", balance: 0.0 },
+  { id: 5, name: "Phil", balance: 18, deposits: [100, 18], withdrawals: [100] },
+];
+
+console.log(getAllDepositsGreaterThanOneHundred(bankAccounts));
 
 
 
