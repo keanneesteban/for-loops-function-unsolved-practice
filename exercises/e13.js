c59bcfa435d794403b0e22a76d07e51767f09bba
keanneesteban <keanneesteban7@gmail.com>
2023-07-19 13:02:01 -0500

    completed exercise 13

--- exercises/e13.js
+++ exercises/e13.js
@@ -5,11 +5,40 @@
 // getAllAccountsWithSumsOfDepositsLess2000(bankAccounts) => [3432, 43242.34, 23432]
 
 export function getAllAccountsWithSumsOfDepositsLess2000(array) {
-  // Your code goes here...
+  var accountsWithSumsLess2000 = [];
 
+  for (var i = 0; i < array.length; i++) {
+    var sum = array[i].deposits ? array[i].deposits.reduce((acc, curr) => acc + curr, 0) : 0;
+
+    if (sum < 2000) {
+      accountsWithSumsLess2000.push(array[i]);
+    }
+  }
+
+  return accountsWithSumsLess2000;
 }
 
+const bankAccounts = [
+  {
+    id: 1,
+    name: "Susan",
+    balance: 100.32,
+    deposits: [150, 30, 221],
+    withdrawals: [110, 70.68, 120],
+  },
+  { id: 2, name: "Morgan", balance: 1100.0, deposits: [1100] },
+  {
+    id: 3,
+    name: "Joshua",
+    balance: 18456.57,
+    deposits: [4000, 5000, 6000, 9200, 256.57],
+    withdrawals: [1500, 1400, 1500, 1500],
+  },
+  { id: 4, name: "Candy", balance: 0.0 },
+  { id: 5, name: "Phil", balance: 18, deposits: [100, 18], withdrawals: [100] }
+];
 
+console.log(getAllAccountsWithSumsOfDepositsLess2000(bankAccounts));
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-13"
