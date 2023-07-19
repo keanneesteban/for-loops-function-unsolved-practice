8e53de82de752aa59fb281d6142cbefef656c334
keanneesteban <keanneesteban7@gmail.com>
2023-07-19 13:44:08 -0500

    completed exercise 14

--- exercises/e14.js
+++ exercises/e14.js
@@ -6,10 +6,43 @@
 // getClientsWithWrongBalance(bankAccounts) => [{ name: 'Name1', balance: 32, ... }, { name: 'Name2', balance: 3523, ... }]
 
 export function getClientsWithWrongBalance(array) {
-  // Your code goes here...
+  var wrongBalance = [];
 
+  for (var i = 0; i < array.length; i++) {
+    var account = array[i];
+    var depositsSum = account.deposits ? account.deposits.reduce((acc, curr) => acc + curr, 0) : 0;
+    var withdrawalsSum = account.withdrawals ? account.withdrawals.reduce((acc, curr) => acc + curr, 0) : 0;
+    var calculatedBalance = depositsSum - withdrawalsSum;
+
+    if (calculatedBalance !== account.balance) {
+      wrongBalance.push(account);
+    }
+  }
+
+  return wrongBalance;
 }
 
+const bankAccounts = [
+  {
+    id: 1,
+    name: "Susan",
+    balance: 100.32,
+    deposits: [150, 30, 221],
+    withdrawals: [110, 70.68, 120],
+  },
+  { id: 2, name: "Morgan", balance: 1100.0, deposits: [1100] },
+  {
+    id: 3,
+    name: "Joshua",
+    balance: 18456.57,
+    deposits: [4000, 5000, 6000, 9200, 256.57],
+    withdrawals: [1500, 1400, 1500, 1500],
+  },
+  { id: 4, name: "Candy", balance: 0.0 },
+  { id: 5, name: "Phil", balance: 18, deposits: [100, 18], withdrawals: [100] }
+];
+
+console.log(getClientsWithWrongBalance(bankAccounts));
 
 
 // === TEST YOURSELF ===
